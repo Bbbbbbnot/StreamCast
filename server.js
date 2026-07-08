bb48552aeef42f523a709a068c159b9bbd2bb9d7
Bbbbbbnot <catai21111@gmail.com>
2026-07-08 14:53:57 +0500

    Update passport module import in server.js

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const express = require('express');
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
 const path = require('path');
-const passport = require('./auth');
-
+const passport = require('./passport');
 const authRoutes = require('./auth');
 const streamRoutes = require('./stream');
 const paymentRoutes = require('./payment');
